aba28019f947c30506a09782ddc5c00a48ec9340
sed-int <nouveau0309@gmail.com>
2025-09-16 21:16:33 +0900

    feat: added user respository

---
+++ src/main/java/com/example/demo_habit/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo_habit.repository;
+
+import com.example.demo_habit.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+}
+
+

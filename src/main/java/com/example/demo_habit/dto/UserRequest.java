040d5bb72d8fd42e438358e8e04b21927b90ceb6
sed-int <nouveau0309@gmail.com>
2025-09-18 22:28:09 +0900

    fix: DTO shouldn't be annotated as component

--- src/main/java/com/example/demo_habit/dto/UserRequest.java
+++ src/main/java/com/example/demo_habit/dto/UserRequest.java
@@ -1,11 +1,13 @@
 package com.example.demo_habit.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.stereotype.Component;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Component
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequest {
     // id and many others
     private String email;

e33525ddafebec5afe4bfb2000892cfff8754145
hcho <hcho@scope.co.kr>
2025-09-19 09:28:49 +0900

    feat: add argument validation w/ 'jakarta.validation'

--- src/main/java/com/example/demo_habit/dto/UserRequest.java
+++ src/main/java/com/example/demo_habit/dto/UserRequest.java
@@ -1,15 +1,23 @@
 package com.example.demo_habit.dto;
 
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequest {
-    // id and many others
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 }
